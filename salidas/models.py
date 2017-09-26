4a68030cd397d8ba2e4fd85202fe36b17d70c999
jukson <juksonvillegas@gmail.com>
2017-09-26 18:14:41 -0500

    ya descuenta y retorna en la app perdidas a los detalles de compra

--- salidas/models.py
+++ salidas/models.py
@@ -4,6 +4,7 @@ from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 from django.db import models
 from productos.models import Producto
+from compras.models import detalle_compra
 
 # Create your models here.
 class Perdida(models.Model):
@@ -31,6 +32,36 @@ class Gasto(models.Model):
     def __str__(self):
         return str(self.fecha)
 
+def descontardc(prod, cant):
+    dcs = detalle_compra.objects.filter(producto=prod, activo=True).order_by('compra__fecha')
+    for dc in dcs:
+        stock = dc.cantidad - dc.unidades_fuera
+        if cant>0:
+            if stock >= cant:
+                dc.unidades_fuera += int(cant)
+                cant = 0
+            else:
+                cant -= stock
+                dc.unidades_fuera = dc.cantidad
+            if dc.cantidad == dc.unidades_fuera:
+                dc.activo=False
+            dc.save()
+
+def regresardc(prod, cant):
+        dcs = detalle_compra.objects.filter(producto=prod).order_by('compra__fecha')
+        for dc in dcs:
+            if cant > 0:
+                if cant>dc.unidades_fuera:
+                    cant -=dc.unidades_fuera
+                else:
+                    dc.unidades_fuera -=cant
+                    cant = 0
+                if dc.cantidad == dc.unidades_fuera:
+                    dc.activo = False
+                else:
+                    dc.activo = True
+                dc.save()
+
 @receiver(post_save, sender=Perdida)
 def descontarstock(sender, instance, **kwargs):
     if kwargs['created']:
@@ -38,6 +69,7 @@ def descontarstock(sender, instance, **kwargs):
         nuevo_stock = instance.cantidad
         producto = get_object_or_404(Producto, pk=pk)
         producto.stock-= int(nuevo_stock)
+        descontardc(producto, nuevo_stock)
         producto.save()
 
 @receiver(post_delete, sender=Perdida)
@@ -46,4 +78,5 @@ def retornarstock(sender, instance, **kwargs):
     nuevo_stock = instance.cantidad
     producto = get_object_or_404(Producto, pk=pk)
     producto.stock+= int(nuevo_stock)
+    regresardc(producto,nuevo_stock)
     producto.save()
